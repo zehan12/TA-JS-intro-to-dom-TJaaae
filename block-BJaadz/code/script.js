6d64efef06205b8d6947b74461ea6e7bbd0caa98
Zehan Khan <zehan9211@gmail.com>
2021-10-22 02:25:04 +0530

    DOM<

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,10 +1,18 @@
 // Select the h1 element and store it in a variable named heading.
 
+let heading = document.querySelector("h1");
+
 // Check the typeof heading and log it.
+
+console.log(typeof heading);
 
 // Change the color of heading to black.
 
+heading.style.color = "black";
+
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
+
+let allHrs = document.querySelectorAll("hr");
 
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
@@ -15,23 +23,47 @@ HINT:
 let allHrsArray = Array.from(allHrs)
 */
 
+let allHrsArray = Array.from(allHrs);
+
 // Set the border of the all the hr elements to "1px solid tomato"
+
+allHrs.style.border = "1px solid tomato"
 
 // Change the background of all the hr to "antiquewhite" using for of loop.
 
+allHrsArray.map((cv)=>{
+  cv.style.color = "antiquewhite";
+});
+
 // Change the 'border-radius' of all the hr to "5px" using array.
+
+allHrsArray.map((cv)=>{
+  cv.style.borderRadius = "5px";
+});
 
 // Change the alignment of the heading(h1) to center.
 
+heading.style.textAlign = "center";
+
 // Change the font size of the heading to 3rem.
 
+heading.style.fontSize = "3rem";
+
 // Change the border of hr with class 'image' to `2px solid purple`.
+
+allHrs.style.
 
 // Hide the box number 17 (last box).
 
 // Change the border of all the hr element from solid to dashed type
 
+allHrsArray.map((cv)=>{
+  allHrs.style.border = "dashed";
+});
+
 // Create a pragraph element and store it in variable named 'para' using `createElement`
+
+
 
 // Change the inner text of para to "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure."
 
